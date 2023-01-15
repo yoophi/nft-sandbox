9ae5fda189ee1e9fb20bed86efeef3f84c8d3b91
Pyunghyuk Yoo <pyunghyuk@likelion.net>
2023-01-15 14:29:30 +0900

    INIT: daisy-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-daisyui";
 
 function App() {
-  return <h1 className="header">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="header">Hello world!</h1>
+      <Button color="primary">Click me!</Button>
+    </>
+  );
 }
 
 export default App;
